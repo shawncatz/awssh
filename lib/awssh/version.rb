8963804d86ae80132c1147ec348498667be24692
Shawn Catanzarite <me@shawncatz.com>
2014-09-08 01:37:49 -0700

    bump version

--- lib/awssh/version.rb
+++ lib/awssh/version.rb
@@ -2,7 +2,7 @@ module Awssh
   module Version
     MAJOR = 0
     MINOR = 1
-    TINY  = 1
+    TINY  = 2
     TAG   = nil
     LIST  = [MAJOR, MINOR, TINY, TAG].compact
     STRING = LIST.join('.')
